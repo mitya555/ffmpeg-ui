5a63a873def43166401be641b8c72eb78bfa889e
Dmitriy Mukhin <mitya_mukhin@hotmail.com>
2023-02-22 21:48:57 -0500

    update dependencies, use commons-compress

--- src/main/java/ffmpeg/FFmpeg.java
+++ src/main/java/ffmpeg/FFmpeg.java
@@ -13,21 +13,31 @@ public class FFmpeg {
 	public static void load(URL baseUrl) {
 		try {
 			baseUrl = new URL(baseUrl, JarLib.getOsSubDir() + "/");
-			final String verFileName = "ffmpeg.build.txt";
-			final String exeName = JarLib.getOsSubDir()
-					.startsWith("win") ? "ffmpeg.exe" : "ffmpeg";
-			String localVer = JarLib.getLocal(verFileName), remoteVer = null;
-			if (localVer != null)
-				remoteVer = JarLib.getUrl(new URL(baseUrl, verFileName));
-			if (localVer == null || remoteVer.compareToIgnoreCase(localVer) != 0) {
+			final var verFileName = "ffmpeg.build.txt";
+			final var exeName = JarLib.getOsSubDir().startsWith("win")
+				? "ffmpeg.exe"
+				: "ffmpeg";
+			var localVer = JarLib.getLocal(verFileName);
+			var remoteVer = JarLib.getUrl(new URL(baseUrl, verFileName));
+			if (localVer != null) localVer = localVer.trim();
+			if (remoteVer != null) remoteVer = remoteVer.trim();
+			if (
+				localVer == null || (remoteVer != null &&
+				remoteVer.compareToIgnoreCase(localVer) != 0)
+			) {
+				// download ffmpeg from remoteVer
 				JarLib.deleteLocal(verFileName);
 				JarLib.deleteLocal(exeName);
+				final var archiveName = remoteVer != null && !remoteVer.isEmpty()
+					? remoteVer
+					: "ffmpeg.zip";
+				exe = JarLib.loadFile(new URL(baseUrl, archiveName), exeName, true);
+				exe.setExecutable(true, true);
+				JarLib.loadFile(new URL(baseUrl, verFileName), verFileName, true);
 			}
-			exe = JarLib.loadFile(new URL(baseUrl, "ffmpeg.zip"), exeName, true);
-			exe.setExecutable(true, true);
-			JarLib.loadFile(new URL(baseUrl, verFileName), verFileName, true);
 		} catch (IOException e) {
 			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 	
--- src/main/java/img_applet/JarLib.java
+++ src/main/java/img_applet/JarLib.java
@@ -2,32 +2,39 @@ package img_applet;
 
 import java.io.*;
 import java.net.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+// import java.util.zip.ZipEntry;
+// import java.util.zip.ZipInputStream;
 
-public class JarLib{
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
 
-  static public boolean loadLib(Class<?> cl, String libname, boolean shared){
-    try{
+public class JarLib {
+
+  static public boolean loadLib(Class<?> cl, String libname, boolean shared) {
+    try {
       loadLibX(cl, libname, shared);
       return true;
-    }catch(Exception e){
+    } catch (Exception e) {
       System.out.println("JarLib.loadLib\n\tException = "+e.getMessage());
       System.err.println("JarLib.loadLib\n\tException = "+e.getMessage());
       e.printStackTrace();
-    }catch(Error e){
+    } catch (Error e) {
       System.out.println("JarLib.loadLib\n\tError = "+e);
       System.err.println("JarLib.loadLib\n\tError = "+e);
       e.printStackTrace();
     }
-    try{                                    // Shouldn't really need to load from system defaults anymore
+    try {                                    // Shouldn't really need to load from system defaults anymore
       System.loadLibrary(libname);
       System.out.println("JarLib.loadLib: Successfully loaded library ["+libname+"] from some default system folder");
       return true;
-    }catch(Exception e){
+    } catch (Exception e) {
       System.out.println("JarLib.loadLib\n\tException = "+e.getMessage());
       System.err.println("JarLib.loadLib\n\tException = "+e.getMessage());
-    }catch(Error e){
+    } catch (Error e) {
       System.out.println("JarLib.loadLib\n\tError = "+e);
       System.err.println("JarLib.loadLib\n\tError = "+e);
       e.printStackTrace();
@@ -35,25 +42,25 @@ public class JarLib{
     return false;
   }
 
-  static private void loadLibX(Class<?> cl, String name, boolean shared)throws IOException, UnsatisfiedLinkError{
-    String libname=System.mapLibraryName(name);
-    URL url = cl.getResource(JarLib.getOsSubDir()+"/"+libname);
-    if(url==null){ 
+  static private void loadLibX(Class<?> cl, String name, boolean shared) throws IOException, UnsatisfiedLinkError {
+    String libname = System.mapLibraryName(name);
+    URL url = cl.getResource(getOsSubDir()+"/"+libname);
+    if (url == null) { 
       throw new UnsatisfiedLinkError(JarLib.class.getName()+".loadLibX: Could not find library ["+libname+"]");
     }
     System.load(getFile(url, libname, shared).getAbsolutePath());
     System.out.println("JarLib.loadLib: Successfully loaded library ["+url+"]");
   }
 
-  static public File loadFile(Class<?> cl, String name, boolean shared)throws IOException, UnsatisfiedLinkError{
-    URL url = cl.getResource(JarLib.getOsSubDir()+"/"+name);
-    if(url==null){ 
+  static public File loadFile(Class<?> cl, String name, boolean shared) throws IOException, UnsatisfiedLinkError {
+    URL url = cl.getResource(getOsSubDir()+"/"+name);
+    if (url == null) { 
       throw new UnsatisfiedLinkError(JarLib.class.getName()+".loadFile: Could not find file ["+name+"]");
     }
     return loadFile(url, name, shared);
   }
 
-  static public File loadFile(URL url, String name, boolean shared)throws IOException, UnsatisfiedLinkError{
+  static public File loadFile(URL url, String name, boolean shared) throws IOException, UnsatisfiedLinkError {
     File tmp = getFile(url, name, shared);
     System.out.println("JarLib.loadFile: Successfully loaded file ["+url+"]");
     return tmp;
@@ -61,43 +68,46 @@ public class JarLib{
 
   static final File tmpdir = new File(System.getProperty("java.io.tmpdir"),"img_applet");
   
-  static private File getFile(URL url, String filename, boolean shared)throws IOException, UnsatisfiedLinkError{
-    try{
+  static private File getFile(URL url, String filename, boolean shared) throws IOException, UnsatisfiedLinkError {
+    try {
       URI uri = new URI(url.toString());    
       String scheme = uri.getScheme();
-      if(scheme.equals("file")){                                  // if on local file system use this copy
+      if (scheme.equals("file")) {  // if on local file system use this copy
         System.load(new File(uri).getAbsolutePath());
         System.out.println("JarLib.getFile: Successfully loaded file ["+url+"] from standard file location");
         return new File(uri);
-      }else if( scheme.equals("jar")                              // make copy in tmp folder on local file system
-            ||  scheme.equals("nbjcl")                            // netbeans: 2009-03-05 jmrunge
-            ||  scheme.equals("http")
-        ){
-        File   dir    = tmpdir;
+      } else if (
+          scheme.equals("jar")   || // make copy in tmp folder on local file system
+          scheme.equals("nbjcl") || // netbeans: 2009-03-05 jmrunge
+          scheme.equals("http")  ||
+          scheme.equals("https")
+      ) {
+        File dir = tmpdir;
         dir.mkdirs();
-        File   tmp;
+        File tmp;
         if (shared) {
-          tmp    = new File(dir, "img_applet_" + filename);
+          tmp = new File(dir, "img_applet_" + filename);
           tmp.createNewFile();
-        }else{
-          File[] files=dir.listFiles();
-          for(int i=0;i<files.length;i++){                          // delete all unused library copies
-            if(files[i].getName().endsWith(filename)){              // if library is still needed we won't be able to delete it
+        } else {
+          File[] files = dir.listFiles();
+          for (int i = 0; i < files.length; i++) {       // delete all unused library copies
+            if (files[i].getName().endsWith(filename)) { // if library is still needed we won't be able to delete it
               files[i].delete();                     
             }
           }
-          tmp    = File.createTempFile("img_applet_",filename,dir);  // System.out.println(tmp.getAbsolutePath());
+          tmp = File.createTempFile("img_applet_",filename,dir);  // System.out.println(tmp.getAbsolutePath());
           tmp.deleteOnExit();
         }
         System.out.println("JarLib.getFile: Jar file location: " + tmp.getAbsolutePath());  
-        if (tmp.length() == 0L)
-          JarLib.extract(tmp,url,filename);
+        if (tmp.length() == 0L) {
+          extract(tmp, url, filename);
+        }
         System.out.println("JarLib.getFile: Successfully loaded file ["+url+"] from jar file location");  
         return tmp;
-      }else{
+      } else {
         throw new UnsatisfiedLinkError(JarLib.class.getName()+".getFile:\n\tUnknown URI-Scheme ["+scheme+"]; Could not load file ["+uri+"]");
       }
-    }catch(URISyntaxException urise){
+    } catch (URISyntaxException urise) {
       throw new UnsatisfiedLinkError(JarLib.class.getName()+".getFile:\n\tURI-Syntax Exception; Could not load file ["+url+"]");
     }
   }
@@ -111,39 +121,72 @@ public class JarLib{
   }
   
   static private void copyStream(OutputStream out, InputStream in) throws IOException {
-    byte[] buffer=new byte[4096];
-    int count=0;
-    while((count=in.read(buffer))>0){
-      out.write(buffer,0,count);
+    byte[] buffer = new byte[4096];
+    int count;
+    while ((count = in.read(buffer)) > 0) {
+      out.write(buffer, 0, count);
     }
   }
   
-  static private void extract(OutputStream out, URL url, String filename)throws IOException{
-    try (InputStream      in  = url.openStream()) {
-      if ("zip".equalsIgnoreCase(getExtension(url.getPath()))) {
-        try (ZipInputStream zip = new ZipInputStream(in)) {
-          ZipEntry entry;
-          while((entry = zip.getNextEntry())!=null) {
-            // if (entry.getName().equalsIgnoreCase(filename)) {
-            if (!entry.isDirectory()) {
-              copyStream(out, zip);
-              break;
+  static private void extract(OutputStream out, URL url, String filename) throws IOException {
+    if (url.getPath().toLowerCase().endsWith(".zip")) {
+      // try (
+      //   var in = url.openStream();
+      //   var zip = new ZipInputStream(in)
+      // ) {
+      //   ZipEntry entry;
+      //   while((entry = zip.getNextEntry()) != null) {
+      //     // if (entry.getName().equalsIgnoreCase(filename)) {
+      //     if (!entry.isDirectory()) {
+      //       copyStream(out, zip);
+      //       break;
+      //     }
+      //   }
+      // }
+      var baos = new ByteArrayOutputStream();
+      extract(baos, url);
+      try (
+        var seekable = new SeekableInMemoryByteChannel(baos.toByteArray());
+        var zipFile = new ZipFile(seekable)
+      ) {
+        baos = null;
+        for (var zipEntry : zipFile.getEntries(filename)) {
+          if (!zipEntry.isDirectory() && zipFile.canReadEntryData(zipEntry)) {
+            try (var in = zipFile.getInputStream(zipEntry)) {
+              copyStream(out, in);
             }
           }
         }
-      } else {
+      }
+    } else if (url.getPath().toLowerCase().endsWith(".tar.xz")) {
+      try (
+        var in = url.openStream();
+        var bi = new BufferedInputStream(in);
+        var xzi = new XZCompressorInputStream(bi);
+        var ai = new TarArchiveInputStream(xzi)
+      ) {
+        ArchiveEntry entry;
+        while ((entry = ai.getNextEntry()) != null) {
+          if (!entry.isDirectory() && filename.equals(entry.getName()) && ai.canReadEntryData(entry)) {
+            IOUtils.copyRange(ai, entry.getSize(), out);
+            break;
+          }
+        }
+      }
+     } else {
+      try (var in = url.openStream()) {
         copyStream(out, in);
       }
     }
   }
   
-  static private void extract(OutputStream out, URL url)throws IOException{
-    try (InputStream      in  = url.openStream()) {
+  static private void extract(OutputStream out, URL url) throws IOException {
+    try (InputStream in = url.openStream()) {
       copyStream(out, in);
     }
   }
 
-  static private void extract(File fn, URL url, String filename)throws IOException{
+  static private void extract(File fn, URL url, String filename) throws IOException {
     try (FileOutputStream out = new FileOutputStream(fn)) {
       extract(out, url, filename);
     }
@@ -158,49 +201,50 @@ public class JarLib{
   static final String charset = "UTF-8"; // "US-ASCII"
   
   static public String getUrl(URL url)throws IOException{
-    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-      extract(out, url);
-      return out.toString(charset);
-    }
+    var out = new ByteArrayOutputStream();
+    extract(out, url);
+    return out.toString(charset);
   }
 
-  static public String getLocal(String filename)throws IOException{
+  static public String getLocal(String filename) throws IOException{
     File file = new File(tmpdir, "img_applet_" + filename);
-    if (!file.exists())
-      return null;
-    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-      extract(out, file.toURI().toURL());
-      return out.toString(charset);
-    }
+    if (!file.exists()) return null;
+    var out = new ByteArrayOutputStream();
+    extract(out, file.toURI().toURL());
+    return out.toString(charset);
   }
-  
-  static public boolean deleteLocal(String filename) { return new File(tmpdir, "img_applet_" + filename).delete(); }
 
-  static public String getOsSubDir(){              // This is where I put my stuff
+  static public boolean deleteLocal(String filename) {
+    return new File(tmpdir, "img_applet_" + filename).delete();
+  }
 
-//    System.err.println("java.library.path = "+System.getProperty("java.library.path"));
-//    System.err.println("os.name = "+System.getProperty("os.name"));
-//    System.err.println("os.arch = "+System.getProperty("os.arch"));
+  static public String getOsSubDir() {              // This is where I put my stuff
 
-    String osname=System.getProperty("os.name");
+    // System.err.println("java.library.path = "+System.getProperty("java.library.path"));
+    // System.err.println("os.name = "+System.getProperty("os.name"));
+    // System.err.println("os.arch = "+System.getProperty("os.arch"));
 
-    if(osname.startsWith("Linux")){
-      String osarch=System.getProperty("os.arch");
-      if(osarch.endsWith("64")){                    // amd64
+    String osname = System.getProperty("os.name");
+
+    if (osname.startsWith("Linux")) {
+      String osarch = System.getProperty("os.arch");
+      if (osarch.endsWith("64")) {                  // amd64
         return "lin64";
-      }else{                                        // x86
+      } else {                                      // x86
         return "lin32";
       }
     }
-    if(osname.startsWith("Windows")){
-      String osarch=System.getProperty("os.arch");  // System.err.println("osarch = "+osarch);
-      if(osarch.endsWith("64")){                    // amd64
+    if (osname.startsWith("Windows")) {
+      String osarch = System.getProperty("os.arch");// System.err.println("osarch = "+osarch);
+      if (osarch.endsWith("64")) {                  // amd64
         return "win64";
-      }else{                                        // x86
+      } else {                                      // x86
         return "win32";
       }
     }
-    if(osname.startsWith("Mac")){ return "mac";}
+    if (osname.startsWith("Mac")) {
+      return "mac";
+    }
     return "";
   }
 
